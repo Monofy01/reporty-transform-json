c2aa4758e48f55a08962335b80d98ecd0ff8fd1c
Brian Rivera <bri_riva@hotmail.com>
2023-07-23 01:03:22 -0600

    [Fixed] correct change for process json in event

--- app.py
+++ app.py
@@ -4,19 +4,8 @@ from src.services.report_services import ReportService
 
 
 def handler(event: dict, context):
-    print(event)
-    print(type(event))
     records = event['Records']
-
     for record in records:
-        print(f"LIST + {type(records)}")
-        print(f"ITEM + {type(record)}")
-        print(record)
-
-        print(f"value record[body]: {record['body']}")
-        print(f"type record[body]: {type(record['body'])}")
-
-        parsed_body_data = json.loads(record['body'])
-        print(f"value record[body]: {parsed_body_data}")
-        print(f"type record[body]: {type(parsed_body_data)}")
-        ReportService.create_xlsx(parsed_body_data)
+        record['body'] = record['body'].replace("\\", "")[1:-1]
+        body = json.loads(record['body'])
+        ReportService.create_xlsx(body)
--- src/services/report_services.py
+++ src/services/report_services.py
@@ -10,8 +10,6 @@ class ReportService:
 
     @staticmethod
     def create_xlsx(request_json):
-        print("POST TYPE")
-        print(type(request_json))
         creator_excel = CreatorXlsx(request_json['excel'])
         creator_excel.new_xlsx()
         S3Client.upload_s3(creator_excel)
