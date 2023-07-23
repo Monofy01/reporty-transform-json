2cb8fbec1df98e288b37d9d9ae9cafc5ff593ba8
Brian Rivera <bri_riva@hotmail.com>
2023-07-22 22:05:38 -0600

    [Fixed] new method for read values v8

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from src.services.report_services import ReportService
 
 
 def handler(event: dict, context):
+    print(event)
+    print(type(event))
     records = event['Records']
-    try:
-        for record in records:
-            record_body_str = record['body']
-            record_body_dict = json.loads(record_body_str)
-            print("PRE TYPE")
-            print(type(record_body_dict))
-            ReportService.create_xlsx(record_body_dict)
-    except Exception as e:
-        print(e)
+
+    for record in records:
+        print(f"LIST + {type(records)}")
+        print(f"ITEM + {type(record)}")
+        print(record)
+        record_body_str = record['body']
+        record_body_dict = json.loads(record_body_str)
+        print(type(record_body_dict))
+        ReportService.create_xlsx(record_body_dict)
