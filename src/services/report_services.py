514a0d56b9fd202c1051dc7ab94f5897219f054b
Brian Rivera <bri_riva@hotmail.com>
2023-07-26 12:48:41 -0600

    [Fix] Se arregla metodo uploads3

--- src/services/report_services.py
+++ src/services/report_services.py
@@ -12,6 +12,6 @@ class ReportService:
     def create_xlsx(request_json):
         creator_excel = CreatorXlsx(request_json)
         binary_data = creator_excel.new_xlsx()
-        S3Client.upload_s3(creator_excel, binary_data, request_json['email'])
+        S3Client.upload_s3(creator_excel, binary_data)
 
 
